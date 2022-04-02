19e964175af47882918b45666b27c702448ce87c
Ali Furkan YILDIZ <57134217+uAliFurkanY@users.noreply.github.com>
2022-04-02 20:51:12 +0300

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -14,8 +14,8 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
-                i.src = "https://github.com/r-PlaceTux/Overlay/raw/main/tuxoverlay.png";
-                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+                i.src = "https://github.com/uAliFurkanY/Overlay/raw/main/vcjoverlay.png";
+                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
                 console.log(i);
                 return i;
             })())
